e95cde2b43dd94ef791e9b690f33d7752ce308f1
Ken Halbert <khalbert@healthgrades.com>
2016-11-02 08:06:46 -0600

    Got rid of unneeded template deps

--- ToDoList/ToDoList.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ToDoList.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ToDoList/ToDoList.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ToDoList.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
